aa1794c5d3e999691f11ea692acce80e03b3bf6f
Jassiiimm <owainatijassim@gmail.com>
2026-06-23 10:53:36 +0300

    Stores hidden to the variables

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
-/*-------------------------------- Constants --------------------------------*/
+/*-------------- Constants --------------*/
+const choicesArr = ["rock" , "paper" , "scissors"]
+/*-------------- Variables --------------*/
+let userChoice = ''
+let computerChoice =''
 
-/*-------------------------------- Variables --------------------------------*/
-let userChoice= ''
+/*------ Cached Element References ------*/
+const choices = document.querySelector('#choices')
 
-/*------------------------ Cached Element References ------------------------*/
-const choices = document.querySelector("#choices")
-console.log(choices)
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
 
-/*----------------------------- Event Listeners -----------------------------*/
-choices.addEventListener("click", function(event) {
-    userchoice= event.target.id
-    console.log("userChoice", userChoice)
+const reset = document.querySelector('#reset')
+
+/*----------- Event Listeners -----------*/
+choices.addEventListener('click', function(event){
+    userChoice = event.target.id
+
+    // we want to hide buttons that are not userChoice
+    if (userChoice === 'rock'){
+        // add hidden class to paper and scissors buttons
+        paper.classList.add('hidden')
+        scissors.classList.add('hidden')
+    } else if (userChoice === 'paper') {
+        rock.classList.add('hidden')
+        scissors.classList.add('hidden')
+    } else {
+        rock.classList.add('hidden')
+        paper.classList.add('hidden')
+    }
+
+    let randomIndex =Math.floor(Math.random() * 3)
+// computer makes the choice
+ computerChoice = choicesArr[randomIndex]
+
+
+    console.log('userChoice: ', userChoice)
+    console.log('computerChoice: ', computerChoice)
 })
-/*-------------------------------- Functions --------------------------------*/
+
+reset.addEventListener('click', function(){
+    paper.classList.remove('hidden')
+    scissors.classList.remove('hidden')
+    rock.classList.remove('hidden')
+})
+
+/*-------------- Functions --------------*/
+
+//computer choice helper function
